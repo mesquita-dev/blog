819935da6d6c165765fb9fd8ebfe88191be25c94
Mesquita Dev <lmesquita2k@gmail.com>
2023-04-21 10:51:43 -0300

    Styling the menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,42 @@
-const Header = () => {
-  let cont = 0;
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import logo_blog from "../../svg/logo_blog.svg";
 
-  return <h1>Header.js {cont}</h1>;
+const Header = () => {
+  const descLogo = "Essa é a logo do Blog";
+
+  return (
+    <>
+      <header className="flex-space-between">
+        <div className="logo">
+          <img src={logo_blog} alt={descLogo}></img>
+        </div>
+
+        <div className="search">
+          <input type="text" name="search" className="input-search" placeholder="Buscar artigos, podcasts, destaques..."></input>
+        </div>
+
+        <ul className="menu">
+          <li>
+            <a href="#" className="nav-link">
+              Categories
+            </a>
+          </li>
+
+          <li>
+            <a href="#" className="nav-link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
 };
 
 export default Header;
