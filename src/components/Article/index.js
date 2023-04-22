9110088df7517921134e6539cd6ab6fea06bcaa6
Mesquita Dev <lmesquita2k@gmail.com>
2023-04-22 13:16:01 -0300

    Adding props in components

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,10 +2,25 @@ import Posts from "../Posts";
 
 const Article = () => {
   return (
-    <>
+    <section className="container">
       <h1>Article</h1>
-      <Posts />
-    </>
+      <div className="container-posts mt-5">
+        <Posts subtitle="tecnologia" title="O guia definitivo sobre o Blog">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellentesque. Ultricies habitasse pretium
+          purus viverr.
+        </Posts>
+
+        <Posts subtitle="fotografia" title="Quais as melhores câmeras para 2023?">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellentesque. Ultricies habitasse pretium
+          purus viverr.
+        </Posts>
+
+        <Posts subtitle="cinema" title="Os 10 filmes com maior bilheteria">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellentesque. Ultricies habitasse pretium
+          purus viverr.
+        </Posts>
+      </div>
+    </section>
   );
 };
 
--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,5 +1,21 @@
-const Posts = () => {
-  return <h1>Post</h1>;
+const Posts = (props) => {
+  return (
+    <div className="post mx-2">
+      <div className="img-post"></div>
+      <h5>{props.subtitle}</h5>
+      <h3>{props.title}</h3>
+      <p className="mt-1">{props.children}</p>
+
+      <div className="flex pt-2">
+        <div className="img-profile"></div>
+
+        <div className="desc-profile ml-3">
+          <h6 className="color-blue">Fulano de tal</h6>
+          <p>Aug 8, 2020 - 8 min read</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Posts;
