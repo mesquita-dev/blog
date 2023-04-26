33d665cb938512f03a4c6a261f210a3eca444906
Mesquita Dev <lmesquita2k@gmail.com>
2023-04-26 10:56:42 -0300

    Utilizing Async/Await and useState

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,38 @@
 import api from "../../services/api";
 import Posts from "../../components/Posts";
 
+import { useState } from "react";
+
 const Main = () => {
-  console.log(api);
+  const [post, setPost] = useState([]);
+
+  async function handleAxios() {
+    try {
+      const response = await api.get("/posts");
+      setPost(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
-      <h1>Main.js</h1>
-      <Posts />
+      <section className="container">
+        <h1>Main.js</h1>
+        <button onClick={handleAxios}>Fazer Requisição</button>
+
+        <div className="container-posts mt-5">
+          {post.map((post) => {
+            return (
+              <>
+                <Posts subtitle={post.subtitle} title={post.title} key={post.id}>
+                  {post.resume}
+                </Posts>
+              </>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 };
